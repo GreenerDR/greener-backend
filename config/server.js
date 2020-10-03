fd20dfb973965bc79d571883ccfafe81aeb24524
Juan Ovalle <juanovalle9@gmail.com>
2020-10-03 15:21:09 -0400

    Initialize Strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6858f48497a55de9b3997ea2efa178d7'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e87ccd7b-4e94-4080-b746-9a3fa0bce5d2'
+};
